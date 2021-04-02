eab309967ad448d460a7a666c16d66e986938a69
Robert Planas <robert.planas.jimenez@estudiantat.upc.edu>
2021-04-02 15:17:02 +0200

    Added parallelism

---
+++ Lab/DynamicStep/parallel_methods.py
@@ -0,0 +1,69 @@
+import time
+import numpy as np
+from sklearn.svm import LinearSVC
+from multiprocessing import Pool
+import random
+
+from svm_rfe import SVM_RFE, SVM_RFE_DYNAMIC_STEP
+
+class DSMethods:
+    def __init__(self, n_features, X_train, y_train, X_test, y_test):
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+        self.n_features = n_features
+
+    def randomSelection(self, CVal=10):
+        start_time = time.time()
+        random_scores_train = {}
+        random_scores_test = {}
+        random_selection = random.sample(range(0, self.n_features), self.n_features)
+
+        for i in range(1, self.n_features, 2):
+            features = random_selection[:i]
+        
+            svm = LinearSVC(C=CVal, max_iter=50000, dual=False)
+            svm.fit(self.X_train[:,features], self.y_train)
+
+            random_scores_train[i] = svm.score(self.X_train[:,features], self.y_train)
+            random_scores_test[i] = svm.score(self.X_test[:,features], self.y_test)
+
+        return random_scores_train, random_scores_test, time.time() - start_time
+
+    def _svm_rfe(self, rfe, XT, yT, Xt, yt):
+        start_time = time.time()
+        rfe.fit(XT, yT)
+        elapsed_time = time.time() - start_time
+
+        train_scores = {}
+        test_scores = {}
+        test_selection = np.argsort(rfe.ranking_)
+
+        for i in range(1, self.n_features, 1):
+            features = test_selection[:i]
+
+            svm = LinearSVC(C=10, max_iter=50000, dual=False)
+            svm.fit(XT[:,features], yT)
+
+            train_scores[i] = svm.score(XT[:,features], yT)
+            test_scores[i] = svm.score(Xt[:,features], yt)
+
+        return train_scores, test_scores, rfe.points_, elapsed_time
+
+    def svm_rfe(self, args):
+        train_index, test_index, step = args
+        XT, Xt = self.X_train[train_index], self.X_train[test_index]
+        yT, yt = self.y_train[train_index], self.y_train[test_index]
+        rfe = SVM_RFE(n_features_to_select=1, step=step)
+        return self._svm_rfe(rfe, XT, yT, Xt, yt)
+
+    def svm_rfe_dynamic_step(self, args):
+        train_index, test_index, percentage = args
+        XT, Xt = self.X_train[train_index], self.X_train[test_index]
+        yT, yt = self.y_train[train_index], self.y_train[test_index]
+        rfe = SVM_RFE_DYNAMIC_STEP(n_features_to_select=1, percentage=percentage)
+        return self._svm_rfe(rfe, XT, yT, Xt, yt)
+    
+
+    
---
+++ Lab/DynamicStep/svm_rfe.py
@@ -0,0 +1,118 @@
+import numpy as np
+from sklearn.svm import LinearSVC
+
+class SVM_RFE():
+    def __init__(self, n_features_to_select, step=4):
+        self.n_features_to_select = n_features_to_select
+        self.step = step
+
+    def fit(self, X0, y):
+        self.scores_ = {}
+        self.points_ = []
+        
+        n_features_to_select = self.n_features_to_select
+        n_features = X0.shape[1]
+        if n_features_to_select is None:
+            n_features_to_select = n_features
+            
+        support_ = np.ones(n_features, dtype=bool)
+        ranking_ = np.ones(n_features, dtype=int)
+    
+        # np.sum(support_) is the number of selected features.
+        # It starts at n_features and decreases every iteration.
+        while np.sum(support_) > n_features_to_select:
+            
+            # Remaining features, represented with a list of indices.
+            features = np.arange(n_features)[support_]
+            X = X0[:, features]
+            self.points_ = [np.sum(support_) - 1] + self.points_
+
+            # Declare and train the SVM
+            #with time_code('SVM #' + str(np.sum(support_))):
+            estimator = LinearSVC(C=10, max_iter=50000, dual=False)
+            estimator.fit(X, y)
+
+            # Get importance and rank them
+            importances = estimator.coef_ ** 2
+            ranks = np.argsort(importances)
+
+            # Flatten ranks, required for Multi-Class Classification.
+            ranks = np.ravel(ranks)
+
+            # Calculate t (step)
+            threshold = min(self.step, np.sum(support_) - n_features_to_select)
+
+            # Eliminate the worse feature
+            for i in range(0, threshold):
+                selected_feature = features[ranks[i]]
+                support_[selected_feature] = False
+                ranking_[np.logical_not(support_)] += 1
+
+            # Record score
+            self.scores_[np.sum(support_)] = estimator.score(X,y)
+
+
+        # Set final attributes
+        self.n_features_ = support_.sum()
+        self.support_ = support_
+        self.ranking_ = ranking_
+
+        return self
+
+class SVM_RFE_DYNAMIC_STEP():
+    def __init__(self, n_features_to_select, percentage=0.3):
+        self.n_features_to_select = n_features_to_select
+        self.percentage = percentage
+
+    def fit(self, X0, y):
+        self.scores_ = {}
+        self.points_ = []
+
+        n_features_to_select = self.n_features_to_select
+        n_features = X0.shape[1]
+        if n_features_to_select is None:
+            n_features_to_select = n_features
+            
+        support_ = np.ones(n_features, dtype=bool)
+        ranking_ = np.ones(n_features, dtype=int)
+    
+        # np.sum(support_) is the number of selected features.
+        # It starts at n_features and decreases every iteration.
+        while np.sum(support_) > n_features_to_select:
+            
+            # Remaining features, represented with a list of indices.
+            features = np.arange(n_features)[support_]
+            X = X0[:, features]
+            self.points_ = [np.sum(support_) - 1] + self.points_
+
+            # Declare and train the SVM
+            estimator = LinearSVC(C=10, max_iter=50000, dual=False)
+            estimator.fit(X, y)
+
+            # Get importance and rank them
+            importances = estimator.coef_ ** 2
+            ranks = np.argsort(importances)
+
+            # Flatten ranks, required for Multi-Class Classification.
+            ranks = np.ravel(ranks)
+
+            # Calculate t (step)
+            min_threshold = np.sum(support_) - n_features_to_select
+            threshold = max(int(self.percentage * min_threshold), 1)
+
+            # Eliminate the worse feature
+            for i in range(0, threshold):
+                selected_feature = features[ranks[i]]
+                support_[selected_feature] = False
+                ranking_[np.logical_not(support_)] += 1
+
+            # Record score
+            self.scores_[np.sum(support_)] = estimator.score(X,y)
+
+
+        # Set final attributes
+        self.n_features_ = support_.sum()
+        self.support_ = support_
+        self.ranking_ = ranking_
+
+        return self
